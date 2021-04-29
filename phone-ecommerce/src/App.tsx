c0c62c94a50271023d0ac572bb837373802b3af5
JunmingGuo <sjunmg@gmail.com>
2021-04-28 17:04:21 -0700

    Set up google font and bootstrap

--- phone-ecommerce/src/App.tsx
+++ phone-ecommerce/src/App.tsx
@@ -1,5 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 const App = () => {
-  return <div className="App">App</div>;
-}
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">column one</div>
+        <div className="col-6">
+          <span>
+            <i className="fas fa-home"></i>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
