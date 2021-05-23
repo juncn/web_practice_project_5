99e0fbaaa415fd042b6f090c30e2fda88b8bc76b
JunmingGuo <sjunmg@gmail.com>
2021-05-22 17:22:29 -0700

    Add fetch product useEffect

--- phone-ecommerce/src/components/ProductList.tsx
+++ phone-ecommerce/src/components/ProductList.tsx
@@ -1,12 +1,31 @@
-import { useAppSelector } from '../hooks';
+import { useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../hooks';
 import Title from './Title';
 import Product from './Product';
 import Modal from './Modal';
+import { setProducts } from '../features/productsSlice';
 
 const ProductList = () => {
   const products = useAppSelector(state => state.products);
   const { id, isModalOpen } = useAppSelector(state => state.modal);
   const product = products.find(el => el.id === id);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!localStorage.getItem('products')) {
+      const fetchProduct = () => {
+        fetch('http://localhost:1800')
+          .then(response => response.json())
+          .then(data => {
+            localStorage.setItem('products', JSON.stringify(data.products));
+            dispatch(setProducts(data.products));
+          });
+      }
+      fetchProduct();
+    } else {
+      localStorage.setItem('products', JSON.stringify(products));
+    }
+  }, [dispatch, products]);
 
   return (
     <div className="py-5">
